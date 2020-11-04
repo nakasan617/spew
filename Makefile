0491c568b3c7d51a4e807f23efcef35a01000820
Yuta Nakamura <nakamura1@cs.uchicago.edu>
2020-11-04 11:09:02 -0600

    took care of symlinks

--- Makefile
+++ Makefile
@@ -4,13 +4,13 @@ SOURCES=$(wildcard src/*.c)
 OBJECTS=$(patsubst src/%.c, src/%.o, $(SOURCES))
 
 main: $(OBJECTS)
-	$(CC) $(CFLAGS) -o $@ $(OBJECTS)	
+	$(CC) $(CFLAGS) -O3 -o $@ $(OBJECTS)	
 
 debug:
 	$(CC) $(CFLAGS) -ggdb -o $@ $(SOURCES)
 
 $(OBJECTS): %.o : %.c
-	$(CC) $(CFLAGS) -c $< -o $@
+	$(CC) $(CFLAGS) -O3 -c $< -o $@
 
 .PHONY: clean
 clean:
--- src/main.c
+++ src/main.c
@@ -37,13 +37,18 @@ size_t recursive_write(char *dirname, FILE *fp_w, size_t offset, list_t *metalis
             continue;
         }
         if((stbuf.st_mode & S_IFMT) == S_IFDIR) {
-            fprintf(stderr, "directory: %s\n", filename_qfd); 
+            //fprintf(stderr, "directory: %s\n", filename_qfd); 
             offset = recursive_write(filename_qfd, fp_w, offset, metalist);
-        } else {
-            fprintf(stderr, "filename: %s\n", filename_qfd);
+        } else if((stbuf.st_mode & S_IFMT) == S_IFCHR) {
+            //fprintf(stderr, "this is character-oriented device file: %s\n", filename_qfd);
+        } else if((stbuf.st_mode & S_IFMT) == S_IFBLK) {
+            //fprintf(stderr, "this is block-oriented device file: %s\n", filename_qfd);
+        } else if((stbuf.st_mode & S_IFMT) == S_IFREG) {
+            //fprintf(stderr, "filename: %s\n", filename_qfd);
             FILE *fp_r = fopen(filename_qfd, "r");
             if(!fp_r) {
                 fprintf(stderr, "could not open %s, exitting...\n", filename_qfd);
+                perror("");
                 exit(1);
             }
             size_t sz = get_size(fp_r);
@@ -51,7 +56,6 @@ size_t recursive_write(char *dirname, FILE *fp_w, size_t offset, list_t *metalis
                 fprintf(stderr, "adding metadata for %s failed, exitting...\n", filename_qfd);
                 exit(1);
             }
-            //print_metadata(metalist);
             
             size_t read;
             size_t wrote;
@@ -61,6 +65,33 @@ size_t recursive_write(char *dirname, FILE *fp_w, size_t offset, list_t *metalis
             }
             offset += sz;
             fclose(fp_r);
+        } else if((stbuf.st_mode & S_IFMT) == S_IFLNK) {
+            fprintf(stderr, "this is symbolic link: %s\n", filename_qfd);
+            FILE *fp_r = fopen(filename_qfd, "r");
+            if(!fp_r) {
+                fprintf(stderr, "symlink didnt open: %s\n", filename_qfd);
+                continue;
+            }
+            size_t sz = get_size(fp_r);
+            if(add_metadata(filename_qfd, sz, offset, metalist)) {
+                fprintf(stderr, "adding metadata for %s failed, exitting...\n", filename_qfd);
+                exit(1);
+            }
+            size_t read;
+            size_t wrote;
+            while((read = fread(page, sizeof(char), 4096, fp_r)) > 0) {
+                wrote = fwrite(page, sizeof(char), read, fp_w);
+                assert(read == wrote);
+            }
+            offset += sz;
+            fclose(fp_r);
+
+        } else if((stbuf.st_mode & S_IFMT) == S_IFSOCK) {
+            //fprintf(stderr, "this is socket: %s\n", filename_qfd);
+        } else if((stbuf.st_mode & S_IFMT) == S_IFIFO) {
+            //fprintf(stderr, "this is FIFO or pipe: %s\n", filename_qfd);
+        } else {
+            //fprintf(stderr, "should not be here exitting...\n");
         }
     }
 
@@ -70,11 +101,7 @@ size_t recursive_write(char *dirname, FILE *fp_w, size_t offset, list_t *metalis
 }
 
 int main(int argc, char **argv) {
-    struct dirent *dp;
-    DIR *dfd;
     char *dir;
-    char page [4096];
-    char *filename_qfd = malloc(PATH_MAX);
     list_t *metalist = init_metadata();
     size_t offset = 0;
     char datafile [PATH_MAX];
@@ -97,59 +124,10 @@ int main(int argc, char **argv) {
        fprintf(stderr, "Usage: %s dirname\n", argv[0]);
        return EXIT_FAILURE; 
     }
-    if((dfd = opendir(dir)) == NULL) {
-        fprintf(stderr, "Cannot open %s\n", dir);
-        return EXIT_SUCCESS;
-    }
 
-    while((dp = readdir(dfd)) != NULL) {
-        struct stat stbuf;
-        size_t len = strlen(dp->d_name);
-        if(!strncmp(dp->d_name, ".", len) || !strncmp(dp->d_name, "..", len) ) {
-            continue;
-        }
-        
-        if(dir[strlen(dir) - 1] == '/') {
-            sprintf(filename_qfd, "%s%s", dir, dp->d_name);
-        } else {
-            sprintf(filename_qfd, "%s/%s", dir, dp->d_name);
-        }
-        
-        if(stat(filename_qfd, &stbuf) == -1) {
-            fprintf(stderr, "Unable to stat file: %s\n", filename_qfd);
-            continue;
-        }
-        if((stbuf.st_mode & S_IFMT) == S_IFDIR) {
-            //fprintf(stderr, "directory: %s\n", filename_qfd); 
-            offset = recursive_write(filename_qfd, fp_w, offset, metalist);
-        } else {
-            //fprintf(stderr, "filename: %s\n", filename_qfd);
-            FILE *fp_r = fopen(filename_qfd, "r");
-            if(!fp_r) {
-                fprintf(stderr, "could not open %s, exitting...\n", filename_qfd);
-                exit(1);
-            }
-            size_t sz = get_size(fp_r);
-            if(add_metadata(filename_qfd, sz, offset, metalist)) {
-                fprintf(stderr, "adding metadata for %s failed, exitting...\n", filename_qfd);
-                exit(1);
-            }
-            print_metadata(metalist);
-            
-            size_t read;
-            size_t wrote;
-            while((read = fread(page, sizeof(char), 4096, fp_r)) > 0) {
-                wrote = fwrite(page, sizeof(char), read, fp_w);
-                assert(read == wrote);
-            }
-            fclose(fp_r);
-            offset += sz;
+    offset = recursive_write(dir, fp_w, offset, metalist);
 
-        }
-    }
     fclose(fp_w);
-    closedir(dfd);
-    free(filename_qfd);
     if(write_metadata(metalist, metafile)) {
         fprintf(stderr, "write metadata failed\n");
         return EXIT_FAILURE;
